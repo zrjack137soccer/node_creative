083aed3ad8b5c04e6e83ee45e8f8d44db0549946
EC2 Default User <ec2-user@ip-172-31-45-231.us-west-2.compute.internal>
2018-11-02 01:14:24 +0000

    Commit 2 Angular

--- creativeLab4/public/javascripts/angular.js
+++ creativeLab4/public/javascripts/angular.js
@@ -4,19 +4,18 @@ myApp.factory('myFactory', myFactory)
 myApp.controller('myCtrl', myCtrl)
 
 function myFactory ($http) {
-var API_ROOT = 'weather';
-var woeid;
+var API_ROOT = '/woeid';
+var cityWoeId;
 return {
     get: function() {
         return $http
-        var woeidSearch = "https://www.metaweather.com/api/location/search/?query=" + $scope.userCity;
-        .get(woeidSearch)
+        .get(API_ROOT)
         .then(function (resp) {
             console.log("Searching Woe ID");
             console.log(resp.data.woeid);
-            return resp.data.woeid
+            return resp.data.woeid;
         });
     
     }
-}
+};
 }
