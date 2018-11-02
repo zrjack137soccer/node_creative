d8286312150579eb20380c4d08e9a98a9ee97660
tchacking <tchbook@gmail.com>
2018-11-02 03:38:47 +0000

    almost done

--- creativeLab4/public/javascripts/angular.js
+++ creativeLab4/public/javascripts/angular.js
@@ -4,18 +4,24 @@ app.controller('myCtrl', function($scope, $http) {
         $scope.woeidSearch = "/woeid?q=" + $scope.userCity;
         $scope.woeID = "";
         $scope.forecastList = [];
-        $http.get($scope.woeidSearch).then(function(response) {
-            $scope.woeID = response["data"][0]["woeid"];
-            $scope.weatherSearch = "/forecast?q=" + $scope.woeID;
-            $http.get($scope.weatherSearch).then(function(response) {
-                $scope.forecastList.push(response["data"]["consolidated_weather"][0]);
-                $scope.forecastList.push(response["data"]["consolidated_weather"][1]);
-                $scope.forecastList.push(response["data"]["consolidated_weather"][2]);
-                $scope.forecastList.push(response["data"]["consolidated_weather"][3]);
-                $scope.forecastList.push(response["data"]["consolidated_weather"][4]);
-                $scope.forecastList.push(response["data"]["consolidated_weather"][5]);
-            });
-        });
-        console.log($scope.forecastList);
+        $http.get($scope.woeidSearch).then(
+            function successCallback(response) {
+                console.log(response);
+                $scope.woeID = response["data"][0]["woeid"];
+                $scope.weatherSearch = "/forecast?q=" + $scope.woeID;
+                $http.get($scope.weatherSearch).then(function(response) {
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][0]);
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][1]);
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][2]);
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][3]);
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][4]);
+                    $scope.forecastList.push(response["data"]["consolidated_weather"][5]);
+                });
+            },
+            function errorCallback(response) {
+                console.log(response);
+                $scope.forecastList.push("City could not be found! :(");
+            }
+        );
     };
 })
