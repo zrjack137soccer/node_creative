71aa2eeb0587e1b1cc8177be282aa962b7abe956
tchacking <tchbook@gmail.com>
2018-11-02 01:21:50 +0000

    slash routes created

--- creativeLab4/routes/index.js
+++ creativeLab4/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var request = require('request');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,9 +7,14 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html', {root : 'public'});
 });
 
+router.get('/woeid', function(req, res, next) {
+    var url = "https://www.metaweather.com/api/location/search/?query=" + req.query.q;
+    request(url).pipe(res);
+});
+
 router.get('/forecast', function(req, res, next) {
-    var url = "dummyWebPage/" + req.query.q;
-    //request(url).pipe(res);
+  var url = "https://www.metaweather.com/api/location/" + req.query.q + "/";
+  request(url).pipe(res);
 });
 
 module.exports = router;
