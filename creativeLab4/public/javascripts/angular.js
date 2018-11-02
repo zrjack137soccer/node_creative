242a7da29ee0e762395abc7b70fb6d1611d20c32
tchacking <tchbook@gmail.com>
2018-11-02 02:51:47 +0000

    Working prototype

--- creativeLab4/public/javascripts/angular.js
+++ creativeLab4/public/javascripts/angular.js
@@ -1,50 +1,21 @@
-/*var myApp = window.angular.module('myApp', []);
-
-myApp.factory('myFactory', myFactory);
-myApp.controller('myCtrl', myCtrl);
-
-function myFactory ($http) {
-var API_ROOT = '/woeid';
-var cityWoeId;
-return {
-    get: function() {
-        return $http
-        .get(API_ROOT)
-        .then(function (resp) {
-            console.log("Searching Woe ID");
-            console.log(resp.data.woeid);
-            return resp.data.woeid;
-        });
-    
-    }
-};
-}
-
-function myCtrl ($scope, myFactory) {
-    
-    $scope.cityWoeId = [];
-    
-    myFactory.get()
-    .then(function(data) {
-        $scope.cityWoeId = data.woeid;
-    });
-}*/
-
 var app = angular.module('myApp', [])
 app.controller('myCtrl', function($scope, $http) {
     $scope.forecast = function() {
         $scope.woeidSearch = "/woeid?q=" + $scope.userCity;
         $scope.woeID = "";
+        $scope.forecastList = [];
         $http.get($scope.woeidSearch).then(function(response) {
-            console.log(response);
             $scope.woeID = response["data"][0]["woeid"];
-            console.log($scope.woeID);
             $scope.weatherSearch = "/forecast?q=" + $scope.woeID;
-            console.log($scope.weatherSearch);
             $http.get($scope.weatherSearch).then(function(response) {
-                console.log(response);
-            })
-        })
-       
-    }
+                $scope.forecastList.push(response["data"]["consolidated_weather"][0]);
+                $scope.forecastList.push(response["data"]["consolidated_weather"][1]);
+                $scope.forecastList.push(response["data"]["consolidated_weather"][2]);
+                $scope.forecastList.push(response["data"]["consolidated_weather"][3]);
+                $scope.forecastList.push(response["data"]["consolidated_weather"][4]);
+                $scope.forecastList.push(response["data"]["consolidated_weather"][5]);
+            });
+        });
+        console.log($scope.forecastList);
+    };
 })
